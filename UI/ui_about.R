03bcd05324a10767f217df1741e9115d95404054
Bakti Siregar <13848017+Bakti-Siregar@users.noreply.github.com>
2020-05-09 08:20:17 +0700

    Update ui_about.R

--- UI/ui_about.R
+++ UI/ui_about.R
@@ -5,8 +5,8 @@ body_about <- dashboardBody(
         box(title = div("About this project", style = "padding-left: 20px", class = "h2"),
           column(
             h3("The Background of COVID-19 Dashboard"),
-            "This COVID-19 Dashboard was build from Shiny packages and developed by Bakti Siregar, M.Sc, Lecturer of Data Scientist at Matana University. This App demonstrates some recent news about the Coronavirus pandemic. 
-            All dataset reads from the John Hopkins dataset, and shows some data related to mortality, recovery, infected, and etc.",
+            "This COVID-19 Dashboard was build from Shiny packages and developed by Bakti Siregar, M.Sc (Professional Data Science and Lectuter at Matana University). This App demonstrates some recent news about the Coronavirus pandemic. 
+            All data was import from the John Hopkins dataset, and shows some data related to mortality, recovery, infected, and etc.",
             tags$br(),
             h3("Words of Wisdom to Fight the COVID-19 Deases"),
             "Please stay at home, distance yourself from others and be healthy. We must not only keep our hands clean
